6d9c89ee46f37797c225433f1ff315e5f64c2110
1024jon <1024jon@gmail.com>
2016-10-19 14:16:10 -0600

    scrollview test

--- edidtest.py
+++ edidtest.py
@@ -2,30 +2,20 @@ import kivy
 import subprocess
 kivy.require('1.9.2') # replace with your current kivy version !
 
-from kivy.app import App
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
+from kivy.app import runTouchApp
 
-
-class MyApp(App):
-    def build(self):
-        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        layout.bind(minimum_height=layout.setter('height'))
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-                   
-        with open("/home/pi/edid.txt") as f:
-            contents = f.read()
-            main_label = Label(text=contents)
-            root.add_widget(main_label)
-            
-            
-
-            
-        return root
+layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+# Make sure the height is such that there is something to scroll.
+layout.bind(minimum_height=layout.setter('height'))
+for i in range(100):
+    btn = Button(text=str(i), size_hint_y=None, height=40)
+    layout.add_widget(btn)
+root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+root.add_widget(layout)
         
 if __name__ == '__main__':
     MyApp().run()
