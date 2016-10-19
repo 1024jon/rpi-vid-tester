74151647116d3d3243b01ce1b1f4154e072be0fe
1024jon <1024jon@gmail.com>
2016-10-19 11:54:35 -0600

    edid.py fix

--- edidtest.py
+++ edidtest.py
@@ -2,8 +2,10 @@ import kivy
 import subprocess
 kivy.require('1.9.2') # replace with your current kivy version !
 
+from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
+
 
 class MyApp(App):
     def build(self):
