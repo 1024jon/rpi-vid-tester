44c06870975bb1059c93101ad42ac8da7e9c62c8
1024jon <1024jon@gmail.com>
2016-10-19 17:33:13 -0600

    popup text

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def readedid(instance):
     subprocess.Popen('/home/pi/rpi-vid-tester/readedid.sh') 
     with open("/home/pi/edid.txt") as f:
         contents = f.read()    
-        popup = Popup(content=Label(text=contents, font_size='8sp'), size=(400, 600))    
+        popup = Popup(title='EDID', content=Label(text=contents, font_size='8sp',halign='center'), size_hint=(None, None), size=(400, 600))    
     popup.open()
 
 
