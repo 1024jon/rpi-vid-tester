9137847475c49e478912c4695e62aa59c023a077
1024jon <1024jon@gmail.com>
2016-10-19 14:29:20 -0600

    label test

--- edidtest.py
+++ edidtest.py
@@ -2,21 +2,26 @@ import kivy
 import subprocess
 kivy.require('1.9.2') # replace with your current kivy version !
 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
 from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
-from kivy.app import runTouchApp
 
-layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-# Make sure the height is such that there is something to scroll.
-layout.bind(minimum_height=layout.setter('height'))
-with open("/home/pi/edid.txt") as f:
-    contents = f.read()
-    layout.add_widget(Label(text=contents))
-
-root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-root.add_widget(layout)
+class MyApp(App):
+    def build(self):
+        layout = AnchorLayout(anchor_x='center', anchor_y='bottom')
+        with layout.canvas.before:
+            Color(.2,.2,.2,1)
+            self.rect = Rectangle(size=(800,600), pos=layout.pos)  
+            
+        with open("/home/pi/edid.txt") as f:
+            contents = f.read()
+            main_label = Label(text=contents, text_size=(800, 600))
+            
+            layout.add_widget(main_label)
+            
+            return layout
         
-runTouchApp(root)
+if __name__ == '__main__':
+    MyApp().run()
