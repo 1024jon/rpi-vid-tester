7a2dfc7fd01bc0432f6bf7af241b17649e00dff2
1024jon <1024jon@gmail.com>
2016-10-19 10:51:53 -0600

    ui tweak

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.uix.image import Image
 from kivy.uix.slider import Slider
 from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
+from kivy.uix.label import Label
 
 def crosshatch(instance):
     subprocess.Popen('/home/pi/rpi-vid-tester/crosshatchvid.sh')
@@ -26,15 +27,14 @@ class MyApp(App):
 
     def build(self):
         # Set up the layout:
-        layout = GridLayout(cols=4, spacing=30, padding=30, row_default_height=30)
+        layout = GridLayout(cols=4, spacing=30, padding=30, row_default_height=20)
 
         # Make the background gray:
         with layout.canvas.before:
             Color(.2,.2,.2,1)
             self.rect = Rectangle(size=(800,600), pos=layout.pos)
 
-        # Instantiate the first UI object (the GPIO input indicator):
-        btn1 = Button(text="CrossHatch")
+        btn1 = Button(text="Crosshatch")
         btn1.bind(on_press=crosshatch)
         btn2 = Button(text="Overscan")
         btn2.bind(on_press=overscan)
@@ -44,6 +44,7 @@ class MyApp(App):
         btn4.bind(on_press=set720)
         btn5 = Button(text="Input")
         
+        layout.add_widget(Label(text="Test Video"))
         layout.add_widget(btn1)
         layout.add_widget(btn2)
         layout.add_widget(btn3)
