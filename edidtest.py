4d32864e98d12a916a6344a9325c9057cfdddd1b
1024jon <1024jon@gmail.com>
2016-10-19 11:59:51 -0600

    color

--- edidtest.py
+++ edidtest.py
@@ -5,7 +5,7 @@ kivy.require('1.9.2') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
-
+from kivy.graphics import Color, Rectangle
 
 class MyApp(App):
     def build(self):
