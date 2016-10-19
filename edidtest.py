aa7206262ec50fa93448fdd2cee6f466163344a2
1024jon <1024jon@gmail.com>
2016-10-19 14:20:40 -0600

    scrollview test

--- edidtest.py
+++ edidtest.py
@@ -4,6 +4,7 @@ kivy.require('1.9.2') # replace with your current kivy version !
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.app import runTouchApp
@@ -11,9 +12,10 @@ from kivy.app import runTouchApp
 layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
 # Make sure the height is such that there is something to scroll.
 layout.bind(minimum_height=layout.setter('height'))
-for i in range(100):
-    btn = Button(text=str(i), size_hint_y=None, height=40)
-    layout.add_widget(btn)
+with open("/home/pi/rpi-vid-tester/edid.txt") as f:
+    contents = f.read()
+    layout.add_widget(Label(text=contents))
+
 root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
 root.add_widget(layout)
         
