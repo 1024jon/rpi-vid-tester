f4b3b95a8aeed4847a98058894d243d7342b0109
1024jon <1024jon@gmail.com>
2016-10-19 18:04:15 -0600

    read3 script fix

--- readedid3.sh
+++ readedid3.sh
@@ -3,4 +3,4 @@
 touch /home/pi/edid.txt /home/pi/edid1.txt /home/pi/edid3.txt
 tvservice -d /home/pi/edid.dat
 edidparser /home/pi/edid.dat > /home/pi/edid.txt
-sed -n '31,55p' < edid.txt > /home/pi/edid3.txt
+sed -n '31,55p' < /home/pi/edid.txt > /home/pi/edid3.txt
